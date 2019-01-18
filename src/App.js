1ff182b6d237cc1e62a58dd2db9ad1347057f8e8
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-01-18 11:31:40 +0800

    with a fixed dropdown from semantic ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import ToDoList from './Components/ToDoList'
-import ValidationForm from './Components/ValidatioForm';
-import { Segment,Card } from 'semantic-ui-react';
-import MyForm from './Components/myForm';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-  
-           <MyForm />
+      
       </div>
+      
+
     );
   }
 }
