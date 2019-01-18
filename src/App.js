3023ec0728772a82184d9294e1549fa39306040c
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-01-18 14:02:18 +0800

    playing with random user api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-
+import FetchRandom from './Components/FetchRandomUser';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <FetchRandom />
       </div>
       
-
     );
   }
 }
